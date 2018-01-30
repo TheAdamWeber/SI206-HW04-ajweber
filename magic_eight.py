de18622425539eb14aaf07eb5f2659bb9763109e
ajweber <ajweber@umich.edu>
2018-01-30 15:07:10 -0500

    Added question check

--- magic_eight.py
+++ magic_eight.py
@@ -2,4 +2,14 @@ def get_question():
     ques = input("What is your question? ")
     return ques
 
+def check_question(ques):
+    if (ques.endswith('?')):
+        return True
+    else:
+        print("I'm sorry, I can only answer questions.")
+        return False
+
+
+
 question = get_question()
+
