bb9edd29fb3068a4969588648db555dcd38218e0
asier berhane <asierberhane@0587358264.wireless.umich.net>
2018-01-30 14:57:26 -0500

    added answers

--- magic_eight.py
+++ magic_eight.py
@@ -1,5 +1,26 @@
+import random
+
 def get_question():
     ques = input("What is your question? ")
     return ques
+
+def answers():
+    response = ['It is certain', 'It is decidedly so', 'Without a doubt',
+                'Yes definitely', 'You may rely on it', 'As I see it, yes','Most likely',
+                'Outlook good',
+                'Yes',
+                'Signs point to yes',
+                'Reply hazy try again',
+                'Ask again later',
+                'Better not tell you now',
+                'Cannot predict now',
+                'Concentrate and ask again',
+                'Don\'t count on it',
+                "My reply is no",
+                'My sources say no',
+                'Outlook not so good',
+                'Very doubtful']
+    return random.choice(response)
+
 
 question = get_question()
